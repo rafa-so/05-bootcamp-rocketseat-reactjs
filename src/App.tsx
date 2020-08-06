22fa226f8c825152f8418e9024c1803bca5fd531
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-08-06 00:34:04 -0300

    Criados componentes de navegação
     - Criado o componente de página da listagem de todos os repositórios
     - Criado o componente que vai apresentar dados específicos do componente escolhido no componente anterior

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => <BrowserRouter><Routes /></BrowserRouter>;
 
 export default App;
